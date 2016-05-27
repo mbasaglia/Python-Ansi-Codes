50568bcc709dab87eef62dbc84268f2900a3cdc3
Mattia Basaglia <mattia.basaglia@gmail.com>
2016-05-27 13:47:04 +0100

    Rect class

--- patsi/widgets/geometry.py
+++ patsi/widgets/geometry.py
@@ -69,11 +69,6 @@ class Point(object):
             lerp(self.y, other.y, factor)
         )
 
-    def in_window(self, curses_window):
-        min = Point(*curses_window.getbegyx(), reversed=True)
-        max = Point(*curses_window.getmaxyx(), reversed=True)
-        return self.x > min.x and self.x < max.x and self.y > min.y and self.y < max.y
-
     def __repr__(self):
         return "(%s, %s)" % (self.x, self.y)
 
@@ -90,3 +85,158 @@ class Point(object):
     def __iter__(self):
         yield self.x
         yield self.y
+
+
+class Rect(object):
+    def __init__(self, **kwargs):
+        if "pos" in kwargs and "size" in kwargs:
+            self.top_left = kwargs["pos"]
+            self.bottom_right = self.top_left + kwargs["size"]
+        elif "top_left" in kwargs and "bottom_right" in kwargs:
+            self.top_left = kwargs["top_left"]
+            self.bottom_right = kwargs["bottom_right"]
+        elif ( "x" in kwargs and "y" in kwargs and
+                "width" in kwargs and "height" in kwargs ):
+            self.top_left = Point(kwargs["x"], kwargs["y"])
+            self.bottom_right = self.top_left + \
+                Point(kwargs["width"], kwargs["height"])
+        elif ( "x1" in kwargs and "y1" in kwargs and
+                "x2" in kwargs and "y2" in kwargs ):
+            self.top_left = Point(kwargs["x1"], kwargs["y1"])
+            self.bottom_right = Point(kwargs["x2"], kwargs["y2"])
+        elif ( "top" in kwargs and "left" in kwargs and
+                "bottom" in kwargs and "right" in kwargs ):
+            self.top_left = Point(kwargs["top"], kwargs["left"])
+            self.bottom_right = Point(kwargs["bottom"], kwargs["right"])
+        else:
+            self.top_left = Point()
+            self.bottom_right = Point()
+
+    def contains(self, point, inclusive=True):
+        if inclusive:
+            return self.top_left.x <= point.x <= self.bottom_right.x and \
+                   self.top_left.y <= point.y <= self.bottom_right.y
+        return self.top_left.x < point.x < self.bottom_right.x and \
+               self.top_left.y < point.y < self.bottom_right.y
+
+    @property
+    def top(self):
+        return self.top_left.y
+
+    @property
+    def left(self):
+        return self.top_left.x
+
+    @property
+    def bottom(self):
+        return self.bottom_right.y
+
+    @property
+    def right(self):
+        return self.bottom_right.x
+
+    @property
+    def top_right(self):
+        return Point(self.right, self.top)
+
+    @property
+    def bottom_left(self):
+        return Point(self.left, self.bottom)
+
+    @property
+    def center(self):
+        return self.top_left.interpolate(self.bottom_right)
+
+    @property
+    def x1(self):
+        return self.top_left.x
+
+    @property
+    def y1(self):
+        return self.top_left.y
+
+    @property
+    def x2(self):
+        return self.bottom_right.x
+
+    @property
+    def y2(self):
+        return self.bottom_right.y
+
+    @property
+    def x(self):
+        return self.top_left.x
+
+    @property
+    def y(self):
+        return self.top_left.y
+
+    @property
+    def width(self):
+        return self.bottom_right.x - self.top_left.x
+
+    @property
+    def height(self):
+        return self.bottom_right.y - self.top_left.y
+
+    @property
+    def size(self):
+        return self.bottom_right - self.top_left
+
+    @top_right.setter
+    def top_right(self, point):
+        self.bottom_right.x = point.x
+        self.top_left.y = point.y
+
+    @bottom_left.setter
+    def bottom_left(self, point):
+        self.top_left.x = point.x
+        self.bottom_right.y = point.y
+
+    @left.setter
+    def left(self, val):
+        self.top_left.x = val
+
+    @top.setter
+    def top(self, val):
+        self.top_left.y = val
+
+    @bottom.setter
+    def bottom(self, val):
+        self.bottom_right.y = val
+
+    @right.setter
+    def right(self, val):
+        self.bottom_right.x = val
+
+    @x.setter
+    def x(self, val):
+        width = self.width
+        self.top_left.x = val
+        self.bottom_right.x = val + width
+
+    @y.setter
+    def y(self, val):
+        height = self.height
+        self.top_left.y = val
+        self.bottom_right.y = val + height
+
+    @width.setter
+    def width(self, val):
+        self.bottom_right.x = self.top_left.x + val
+
+    @height.setter
+    def height(self, val):
+        self.bottom_right.y = self.top_left.y + val
+
+    @size.setter
+    def size(self, point):
+        self.width = point.x
+        self.height = point.y
+
+    @center.setter
+    def center(self, point):
+        size = self.size
+        self.top_left.x = point.x - size.x / 2
+        self.top_left.y = point.y - size.y / 2
+        self.bottom_right = self.top_left + size
--- test/test-widgets.py
+++ test/test-widgets.py
@@ -76,4 +76,110 @@ class TestPoint(test_common.TestCase):
         self.assertEquals(str(point), str(tuple(point)))
 
 
+class TestRect(test_common.TestCase):
+    def assert_rect(self, rect, x1, y1, x2, y2):
+        self.assertEquals(rect.top_left, geo.Point(x1, y1))
+        self.assertEquals(rect.bottom_right, geo.Point(x2, y2))
+
+    def test_ctor(self):
+        tl = geo.Point(1, 2)
+        br = geo.Point(11, 22)
+        sz = geo.Point(10, 20)
+        self.assert_rect(geo.Rect(pos=tl, size=sz), 1, 2, 11, 22)
+        self.assert_rect(geo.Rect(top_left=tl, bottom_right=br), 1, 2, 11, 22)
+        self.assert_rect(
+            geo.Rect(x=tl.x, y=tl.y, width=sz.x, height=sz.y),
+            1, 2, 11, 22
+        )
+        self.assert_rect(
+            geo.Rect(x1=tl.x, y1=tl.y, x2=br.x, y2=br.y),
+            1, 2, 11, 22
+        )
+        self.assert_rect(
+            geo.Rect(top=tl.x, left=tl.y, bottom=br.x, right=br.y),
+            1, 2, 11, 22
+        )
+        self.assert_rect(geo.Rect(), 0, 0, 0, 0)
+
+    def test_contains(self):
+        rect = geo.Rect(x1=10, y1=10, x2=20, y2=20)
+        pin = geo.Point(12, 13)
+        pout = geo.Point(14, 2)
+        pbound = geo.Point(14, 10)
+
+        self.assertTrue(rect.contains(pin))
+        self.assertFalse(rect.contains(pout))
+        self.assertTrue(rect.contains(pbound))
+
+        self.assertTrue(rect.contains(pin, False))
+        self.assertFalse(rect.contains(pout, False))
+        self.assertFalse(rect.contains(pbound, False))
+
+    def test_property_getters(self):
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        self.assertEquals(rect.top_left, geo.Point(1, 2))
+        self.assertEquals(rect.bottom_right, geo.Point(11, 32))
+        self.assertEquals(rect.top_right, geo.Point(11, 2))
+        self.assertEquals(rect.bottom_left, geo.Point(1, 32))
+        self.assertEquals(rect.center, geo.Point(6, 17))
+
+        self.assertEquals(rect.top, 2)
+        self.assertEquals(rect.left, 1)
+        self.assertEquals(rect.bottom, 32)
+        self.assertEquals(rect.right, 11)
+
+        self.assertEquals(rect.x1, 1)
+        self.assertEquals(rect.y1, 2)
+        self.assertEquals(rect.x2, 11)
+        self.assertEquals(rect.y2, 32)
+
+        self.assertEquals(rect.x, 1)
+        self.assertEquals(rect.y, 2)
+        self.assertEquals(rect.width, 10)
+        self.assertEquals(rect.height, 30)
+        self.assertEquals(rect.size, geo.Point(10, 30))
+
+    def test_property_setters(self):
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.top_left = geo.Point(10, 20)
+        self.assert_rect(rect, 10, 20, 11, 32)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.bottom_right = geo.Point(30, 40)
+        self.assert_rect(rect, 1, 2, 30, 40)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.top_right = geo.Point(10, 20)
+        self.assert_rect(rect, 1, 20, 10, 32)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.bottom_left = geo.Point(40, 20)
+        self.assert_rect(rect, 40, 2, 11, 20)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.left = 4
+        rect.top = 5
+        rect.right = 6
+        rect.bottom = 7
+        self.assert_rect(rect, 4, 5, 6, 7)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.x = 101
+        rect.y = 102
+        self.assert_rect(rect, 101, 102, 111, 132)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.width = 100
+        rect.height = 200
+        self.assert_rect(rect, 1, 2, 101, 202)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.size = geo.Point(100, 200)
+        self.assert_rect(rect, 1, 2, 101, 202)
+
+        rect = geo.Rect(x1=1, y1=2, x2=11, y2=32)
+        rect.center = geo.Point(0, 0)
+        self.assert_rect(rect, -5, -15, 5, 15)
+
+
 test_common.main()
